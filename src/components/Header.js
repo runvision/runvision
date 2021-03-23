c362f08a7c8eaef00ffe680f66f554fac87942c9
Artur <admin@deswolf.com>
2021-03-23 19:38:46 +0000

    init changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,15 +3,14 @@ import React from 'react'
 
 const Header = props => (
   <header id="header" style={props.timeout ? { display: 'none' } : {}}>
-    <div className="logo">
+    {/* <div className="logo">
       <span className="icon fa-diamond"></span>
-    </div>
+    </div> */}
     <div className="content">
       <div className="inner">
-        <h1>Dimension</h1>
+        <h1>RunVision</h1>
         <p>
-          A fully responsive site template designed by{' '}
-          <a href="https://html5up.net">HTML5 UP</a> and released
+          A fully responsive site template designed by HTML UP and released
           <br />
           for free under the{' '}
           <a href="https://html5up.net/license">Creative Commons</a> license.
